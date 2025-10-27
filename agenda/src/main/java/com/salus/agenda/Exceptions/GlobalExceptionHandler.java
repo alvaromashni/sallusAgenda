36eae4f8cf558f8475f21389d19f2e70bbfcc3a2
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-10-27 13:01:10 -0300

    Criada a classe GlobalExceptionHandler

---
+++ agenda/src/main/java/com/salus/agenda/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.salus.agenda.Exceptions;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handleResourceNotFound(ResourceNotFoundException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put(null, body);
+    }
+}
