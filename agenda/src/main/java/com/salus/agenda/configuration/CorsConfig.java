a6c2159e5263885b937a565a0da82e08298ddeec
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-12-11 12:49:42 -0300

    fix: cors config fixed

--- agenda/src/main/java/com/salus/agenda/configuration/CorsConfig.java
+++ agenda/src/main/java/com/salus/agenda/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://salusagenda.vercel.app/")
+                        .allowedOrigins("https://salusagenda.vercel.app")
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowCredentials(true);
