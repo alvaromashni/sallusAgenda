3a248c5783f337d2ff90b08d20378c06f1115fe3
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-12-11 13:02:04 -0300

    fix: cors config fixed

--- agenda/src/main/java/com/salus/agenda/configuration/CorsConfig.java
+++ agenda/src/main/java/com/salus/agenda/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://salusagenda.vercel.app")
+                        .allowedOrigins("https://salusagenda.vercel.app","http://localhost:5173")
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowCredentials(true);
--- agenda/src/main/java/com/salus/agenda/configuration/security/SecurityConfiguration.java
+++ agenda/src/main/java/com/salus/agenda/configuration/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.salus.agenda.configuration.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,6 +21,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/patient/**",
